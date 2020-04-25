c4f4a2ed5eca63472bf9a32316a4af36e5bcfc5e
mahmoud2121996 <mahmoudasim74@gmail.com>
2020-04-25 16:46:29 +0200

    update updateing user profile

--- users/forms.py
+++ users/forms.py
@@ -35,26 +35,39 @@ class LoginForm(forms.ModelForm):
 
 
 class UpdateUserForm(forms.ModelForm):
-    date_birth = forms.DateField(required=False)
-    photo = forms.FileField(required=False)
+
+    YEARS= [x for x in range(1940,2021)]
+    date_birth = forms.DateField(label="enter your birthdate",widget=forms.SelectDateWidget(years=YEARS),initial='1891-1-1')
+    photo = forms.ImageField(required=False)
     class Meta:
         model = Users
         fields = ('first_name','last_name','phone','photo','date_birth','facebook_link','country')
 
 
-    def clean_first_name(self):
-        if self.is_valid():
-            first_name = self.cleaned_data['first_name'].strip()
-            if first_name :
-                return first_name
-            else:
-                raise forms.ValidationError('first name can\'t be empty')
+    # def clean_first_name(self):
+    #     if self.is_valid():
+    #         first_name = self.cleaned_data['first_name'].strip()
+    #         if first_name :
+    #             return first_name
+    #         else:
+    #             raise forms.ValidationError('first name can\'t be empty')
             
 
-    def clean_last_name(self):
+    # def clean_last_name(self):
+    #     if self.is_valid():
+    #         last_name = self.cleaned_data['last_name'].strip()
+    #         if last_name :
+    #             return last_name
+    #         else:
+    #             raise forms.ValidationError('last name can\'t be empty')
+
+    
+    def clean_photo(self):
         if self.is_valid():
-            last_name = self.cleaned_data['last_name'].strip()
-            if last_name :
-                return last_name
+            photo = self.cleaned_data['photo']
+            if photo:
+                print("instance photo is :",self.instance.photo)
+                print("photo is :",self.cleaned_data['photo'])
+                return photo
             else:
-                raise forms.ValidationError('last name can\'t be empty')
+                raise forms.ValidationError('please upload a valid photo')

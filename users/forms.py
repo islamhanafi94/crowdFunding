c0237df286be1d4b0c28190d12112ff8541542cd
mahmoud2121996 <mahmoudasim74@gmail.com>
2020-04-27 15:10:40 +0200

    update users urls and user update form...

--- users/forms.py
+++ users/forms.py
@@ -44,3 +44,29 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = Users
         fields = ('first_name','last_name','phone','photo','date_birth','facebook_link','country')
+
+    def clean_country(self):
+        if self.is_valid():
+            country = self.cleaned_data['country']
+            if country:
+                return country
+            else : 
+                return None
+
+
+    def clean_facebook_link(self):
+        if self.is_valid():
+            facebook_link = self.cleaned_data['facebook_link']
+            if facebook_link:
+                return facebook_link
+            else : 
+                return None
+    
+
+    def clean_date_birth(self):
+        if self.is_valid():
+            date_birth = self.cleaned_data['date_birth']
+            if date_birth:
+                return date_birth
+            else : 
+                return None
